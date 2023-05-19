bc0e69cb42fcf25aaae4ce7cabec635d674f7486
samusada <samusada95@gmail.com>
2023-05-19 16:17:43 +0200

    2023-05-19

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reproductor_musica',
+    version='0.01',
+    packages=[''],
+    url='https://github.com/SamuSada',
+    license='Libre',
+    author='SamuSada',
+    author_email='samusada95@gmail.com',
+    description='Reproductor de música'
+)
